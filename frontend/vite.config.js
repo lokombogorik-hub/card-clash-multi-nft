9947ac6d1bbbb1e32ac5649b1018908fb02ffed3
CardClashDev <puksmerdasij@gmail.com>
2026-02-17 12:57:50 +0300

    fix: install all browserify polyfills, remove problematic aliases

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,37 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      buffer: "buffer",
-      stream: "stream-browserify",
-      crypto: "crypto-browserify",
-      util: "util",
-      assert: "assert",
-      process: "process/browser",
-      path: "path-browserify",
-    },
-  },
   define: {
     global: "globalThis",
     "process.env": {},
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-    },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      external: ["fsevents"],
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 });
